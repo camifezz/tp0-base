9203902671f0919677a399a42252a9534ec61d21
Camila Fernandez <camila2007@live.com.ar>
2026-03-16 21:21:47 -0300

    prueba

--- generar-compose.sh
+++ generar-compose.sh
@@ -38,6 +38,11 @@ if [ "$client_count" -gt 0 ]; then
     entrypoint: /client
     environment:
       - CLI_ID=${i}
+      - NOMBRE=Santiago Lionel
+      - APELLIDO=Lorca
+      - DOCUMENTO=3090446${i}
+      - NACIMIENTO=1999-03-17
+      - NUMERO=757${i}
     volumes:
       - ./client/config.yaml:/config.yaml:ro
     networks:
